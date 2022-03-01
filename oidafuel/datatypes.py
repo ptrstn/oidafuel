228f870529c021a67a87844ad31f11d972920d21
Peter Stein <peterstein@protonmail.com>
2022-03-02 00:12:31 +0100

    Implement Municipality, District and State

--- oidafuel/datatypes.py
+++ oidafuel/datatypes.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum
 
 
@@ -10,3 +11,129 @@ class FuelType(str, Enum):
 class RegionType(str, Enum):
     BUNDESLAND = "BL"
     PB = "PB"
+
+
+@dataclass
+class Region:
+    region_code: int
+    region_type: RegionType
+    name: str
+    sub_regions: list["Region"]
+    postal_codes: list[str] = None
+    cities: list[str] = None
+
+    @classmethod
+    def from_response_dict(cls, response_dict: dict) -> "Region":
+        mapping = {
+            "code": "region_code",
+            "type": "region_type",
+            "name": "name",
+            "subRegions": "sub_regions",
+            "postalCodes": "postal_codes",
+            "cities": "cities",
+        }
+        kwargs = {mapping[key]: value for key, value in response_dict.items()}
+
+        if kwargs["sub_regions"]:
+            kwargs["sub_regions"] = [
+                cls.from_response_dict(sub_region)
+                for sub_region in kwargs["sub_regions"]
+            ]
+
+        return cls(**kwargs)
+
+
+@dataclass
+class Municipality:
+    """
+    Gemeinde (g)
+
+    A Municipality (German: Gemeinde) is the administrative division
+    encompassing a single village, town, or city in Austria.
+
+    - postal_code (str): Postal code of the municipality (german: Postleitzahl) [p]
+    - name (str): Name of the municipality [n]
+    - longitude (float): Longitude coordinate (german: Längengrad) [l]
+    - latitude (float): Latitude coordinate (german: Breitengrad) [b]
+    """
+
+    postal_code: str  # Postleitzahl p
+    name: str  # Name n
+    longitude: float  # Längengrad l
+    latitude: float  # Breitengrad b
+
+    @classmethod
+    def from_response_dict(cls, response_dict: dict) -> "Municipality":
+        mapping = {
+            "p": "postal_code",
+            "n": "name",
+            "l": "longitude",
+            "b": "latitude",
+        }
+        kwargs = {mapping[key]: value for key, value in response_dict.items()}
+        return cls(**kwargs)
+
+
+@dataclass
+class District:
+    """
+    Bezirk (b)
+
+    A district (German: Bezirk) is a second-level division
+    of the executive arm of the country's government in Austria.
+
+    - region_code (int): Postal code of the municipality [c]
+    - municipalities (list[Municipality]): List of the municipalities (Gemeinden) [g]
+    - name (str): Name of the district [n]
+    """
+
+    region_code: int  # Code c
+    municipalities: list[Municipality]  # Gemeinden g
+    name: str  # Name n
+
+    @classmethod
+    def from_response_dict(cls, response_dict: dict) -> "District":
+        mapping = {
+            "c": "region_code",
+            "g": "municipalities",
+            "n": "name",
+        }
+        kwargs = {mapping[key]: value for key, value in response_dict.items()}
+        if kwargs["municipalities"]:
+            kwargs["municipalities"] = [
+                Municipality.from_response_dict(municipality)
+                for municipality in kwargs["municipalities"]
+            ]
+        return cls(**kwargs)
+
+
+@dataclass
+class State:
+    """
+    Bundesland (b)
+
+    Austria is a made up of nine states (German: Bundesländer)
+
+    - districts (list[District]): b List of the districts (Bezirke) in the state [b]
+    - region_code (int): Postal code of the state [c]
+    - name (str): Name of the district [n]
+    """
+
+    districts: list[District]  # Bezirke b
+    region_code: int  # Code c
+    name: str  # Name n
+
+    @classmethod
+    def from_response_dict(cls, response_dict: dict) -> "State":
+        mapping = {
+            "b": "districts",
+            "c": "region_code",
+            "n": "name",
+        }
+        kwargs = {mapping[key]: value for key, value in response_dict.items()}
+        if kwargs["districts"]:
+            kwargs["districts"] = [
+                District.from_response_dict(district)
+                for district in kwargs["districts"]
+            ]
+        return cls(**kwargs)
---
+++ tests/test_datatypes.py
@@ -0,0 +1,126 @@
+from oidafuel.datatypes import Region, RegionType, Municipality, District, State
+
+
+class TestRegion:
+    def test_from_response_dict(self):
+        region_dict = {
+            "code": 9,
+            "type": "BL",
+            "name": "Wien",
+            "subRegions": [
+                {
+                    "code": 900,
+                    "type": "PB",
+                    "name": "Wien(Stadt)",
+                    "subRegions": [],
+                },
+                {
+                    "code": 901,
+                    "type": "PB",
+                    "name": "Wien  1.,Innere Stadt",
+                    "subRegions": [],
+                    "postalCodes": ["1010"],
+                    "cities": ["Wien"],
+                },
+                {
+                    "code": 902,
+                    "type": "PB",
+                    "name": "Wien  2.,Leopoldstadt",
+                    "subRegions": [],
+                    "postalCodes": ["1020"],
+                    "cities": ["Wien"],
+                },
+            ],
+            "postalCodes": [
+                "1010",
+                "1020",
+            ],
+            "cities": ["Wien"],
+        }
+        region = Region.from_response_dict(region_dict)
+        assert region.region_code == 9
+        assert region.region_type == RegionType.BUNDESLAND
+        assert len(region.sub_regions) == 3
+        assert region.sub_regions[0].region_code == 900
+        assert region.sub_regions[0].region_type == RegionType.PB
+        assert region.sub_regions[0].name == "Wien(Stadt)"
+        assert not region.sub_regions[0].sub_regions
+        assert region.sub_regions[1].region_code == 901
+        assert region.sub_regions[1].region_type == RegionType.PB
+        assert region.sub_regions[1].name == "Wien  1.,Innere Stadt"
+        assert not region.sub_regions[1].sub_regions
+        assert not region.sub_regions[2].sub_regions
+
+
+def test_municipality_from_response_dict():
+    response_dict = {"p": "1190", "n": "Wien", "l": 16.34565761, "b": 48.24574918}
+    municipality = Municipality.from_response_dict(response_dict)
+    assert municipality.postal_code == "1190"
+    assert municipality.name == "Wien"
+    assert municipality.longitude == 16.34565761
+    assert municipality.latitude == 48.24574918
+
+
+def test_district_from_response_dict():
+    response_dict = {
+        "c": 909,
+        "n": "Wien  9.,Alsergrund",
+        "g": [{"p": "1090", "n": "Wien", "l": 16.35787343, "b": 48.22326154}],
+    }
+    district = District.from_response_dict(response_dict)
+    assert district.region_code == 909
+    assert district.name == "Wien  9.,Alsergrund"
+    assert len(district.municipalities) == 1
+    assert district.municipalities[0].postal_code == "1090"
+    assert district.municipalities[0].name == "Wien"
+    assert district.municipalities[0].longitude == 16.35787343
+    assert district.municipalities[0].latitude == 48.22326154
+
+    response_dict = {"c": 909, "n": "Wien  9.,Alsergrund", "g": []}
+    district = District.from_response_dict(response_dict)
+    assert district.region_code == 909
+    assert district.name == "Wien  9.,Alsergrund"
+    assert len(district.municipalities) == 0
+
+
+def test_state_from_response_dict():
+    response_dict = {
+        "c": 5,
+        "n": "Salzburg",
+        "b": [
+            {
+                "c": 501,
+                "n": "Salzburg(Stadt)",
+                "g": [
+                    {
+                        "p": "5020",
+                        "n": "Salzburg",
+                        "l": 13.03999414,
+                        "b": 47.80459405,
+                    },
+                    {
+                        "p": "5023",
+                        "n": "Salzburg-Gnigl",
+                        "l": 13.0736893,
+                        "b": 47.81883887,
+                    },
+                ],
+            },
+        ],
+    }
+    state = State.from_response_dict(response_dict)
+    assert state.region_code == 5
+    assert state.name == "Salzburg"
+    assert state.districts[0].name == "Salzburg(Stadt)"
+    assert state.districts[0].region_code == 501
+    assert state.districts[0].municipalities[1].name == "Salzburg-Gnigl"
+
+    response_dict = {
+        "c": 99,
+        "n": "Problems",
+        "b": [],
+    }
+    state = State.from_response_dict(response_dict)
+    assert state.region_code == 99
+    assert state.name == "Problems"
+    assert not state.districts
