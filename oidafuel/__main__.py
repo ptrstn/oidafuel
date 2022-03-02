6f6bd46bdd388f94ae20bd54b36d2af3ef3ac30f
Peter Stein <peterstein@protonmail.com>
2022-03-02 05:09:08 +0100

    Implement command line functionality

--- oidafuel/__main__.py
+++ oidafuel/__main__.py
@@ -1,6 +1,8 @@
 import argparse
+import sys
 
 from oidafuel import __version__
+from oidafuel.core import get_gas_prices_by_region, get_gas_prices_by_address
 from oidafuel.econtrol import (
     get_regions,
 )
@@ -17,6 +19,16 @@ def parse_arguments():
 
     parser.add_argument("--list-regions", action="store_true", help="List Regions")
 
+    parser.add_argument(
+        "--fuel-type",
+        choices=["SUP", "DIE", "GAS"],
+        help="SUP (Super 95), DIE (Diesel) or GAS (Compressed natural gas)",
+    )
+
+    location_group = parser.add_mutually_exclusive_group()
+    location_group.add_argument("--region", help="Region code")
+    location_group.add_argument("--address", help="Address")
+
     return parser.parse_args()
 
 
@@ -32,6 +44,32 @@ def main():
                     f"{sub_region.name:30} "
                     f"[{sub_region.region_type}]"
                 )
+        return
+
+    region = args.region
+    address = args.address
+    fuel_type = args.fuel_type
+
+    if not region and not address:
+        print("Please specify the region or address")
+        sys.exit()
+
+    if not fuel_type:
+        print("Please specify the fuel type")
+        sys.exit()
+
+    if region:
+        gas_prices = get_gas_prices_by_region(int(region), fuel_type)
+    else:
+        gas_prices = get_gas_prices_by_address(address, fuel_type)
+
+    for gas_price in gas_prices:
+        print(
+            f"{gas_price.price:1.4} "
+            f"{gas_price.name}, "
+            f"{gas_price.postal_code} {gas_price.city}, "
+            f"{gas_price.address} "
+        )
 
 
 if __name__ == "__main__":
