da03f38a31c3d52d2419e5348bec789c615879b1
Peter Stein <peterstein@protonmail.com>
2022-03-03 05:43:33 +0100

    Increase code coverage

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,16 +1,26 @@
-from oidafuel.core import get_gas_prices_by_address, get_gas_prices_by_region
+from oidafuel.core import (
+    get_gas_stations_by_address,
+    get_gas_stations_by_region,
+    gas_stations_to_gas_prices,
+)
 from oidafuel.datatypes import FuelType
 
 
-def test_get_gas_prices_by_address():
+def test_get_gas_stations_by_address():
     address_text = "Molkereistrasse 1, Wien"
     fuel_type = FuelType.SUPER_95
-    prices = get_gas_prices_by_address(address_text, fuel_type)
+    stations = get_gas_stations_by_address(address_text, fuel_type)
+    assert stations
+    prices = gas_stations_to_gas_prices(stations)
     assert prices
+    assert len(stations) > len(prices)
 
 
-def test_get_gas_prices_by_region():
+def test_get_gas_stations_by_region():
     region_code = 920
     fuel_type = FuelType.DIESEL
-    prices = get_gas_prices_by_region(region_code, fuel_type)
+    stations = get_gas_stations_by_region(region_code, fuel_type)
+    assert stations
+    prices = gas_stations_to_gas_prices(stations)
     assert prices
+    assert len(stations) >= len(prices)
